9b097978bf557b8f7489ab2dfa000645d4e97347
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2025-12-11 20:12:54 +0000

    code commit

--- src/Y25/D11.hs
+++ src/Y25/D11.hs
@@ -1,20 +1,47 @@
+{-# LANGUAGE TupleSections #-}
+
 module Y25.D11
     ( parse
     , solve1
     , solve2
     ) where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 
-type Problem = [Int]
+type Node = String
+
+type Problem = Map Node [Node]
 type Answer = Int
 
 
+parseline :: String -> [(Node, Node)]
+parseline line =
+    let from = take 3 line
+        dest = words $ drop 5 line
+    in map (, from) dest
+
 parse :: String -> Problem
-parse = undefined
+parse =
+    foldl (\z (k,a) -> Map.insertWith (++) k [a] z) Map.empty
+    . concatMap parseline
+    . lines
 
 
 solve1 :: Problem -> Answer
-solve1 = undefined
+solve1 input =
+    go [] from
+    where
+        from = "out"
+        goal = "you"
+        go :: [Node] -> Node -> Int
+        go visited node
+            | node == goal = 1
+            | node `elem` visited = 0
+            | otherwise = sum . map (go (node:visited)) $ nodes
+            where
+                nodes = fromMaybe [] $ input Map.!? node
 
 
 solve2 :: Problem -> Answer
