c4dd420b7b60c1db3da51f63902f42f549633da6
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2025-12-12 08:11:50 +0000

    some experiments with caching again

--- src/Y25/D11.hs
+++ src/Y25/D11.hs
@@ -9,6 +9,9 @@ module Y25.D11
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
+import Data.Set (Set, member)
+import qualified Data.Set as Set
+import qualified Data.IntMap.Merge.Strict as Map (merge)
 
 type Node = String
 
@@ -20,7 +23,7 @@ parseline :: String -> [(Node, Node)]
 parseline line =
     let from = take 3 line
         dest = words $ drop 5 line
-    in map (, from) dest
+    in map (from, ) dest
 
 parse :: String -> Problem
 parse =
@@ -31,18 +34,53 @@ parse =
 
 solve1 :: Problem -> Answer
 solve1 input =
-    go [] from
+    go Map.empty from Map.! from
     where
-        from = "out"
-        goal = "you"
-        go :: [Node] -> Node -> Int
+        from = "svr"
+        goal = "out"
+        go :: Map Node Int -> Node -> Map Node Int
         go visited node
-            | node == goal = 1
-            | node `elem` visited = 0
-            | otherwise = sum . map (go (node:visited)) $ nodes
+            | node == goal = Map.insert node 1 Map.empty
+            | Map.member node visited = Map.update (\a -> if a == 0 then Nothing else Just (a + 1)) node visited
+            | null nodes = Map.insert node 0 visited
+            | otherwise = foldl (\v n ->
+                let new_v = go v n
+                in Map.insertWith (+) node (new_v Map.! n) new_v
+                ) visited nodes
             where
                 nodes = fromMaybe [] $ input Map.!? node
 
 
 solve2 :: Problem -> Answer
-solve2 = undefined
+solve2 input =
+    go Set.empty from
+    where
+
+        from = "svr"
+        goal = "out"
+        go :: Set Node -> Node -> Int
+        go visited node
+            | node == goal =
+                if "dac" `member` visited && "fft" `member` visited then 1 else 0
+            | node `member` visited = error "loop"
+            | otherwise = sum . map (go (node `Set.insert` visited)) $ nodes
+            where
+                nodes = fromMaybe [] $ input Map.!? node
+
+
+{-
+solve1wrong :: Problem -> Answer
+solve1wrong input =
+    go (0, Map.empty) from Map.! from
+    where
+        from = "you"
+        goal = "out"
+        go :: (Int, Map Node Int) -> Node -> (Int, Map Node Int)
+        go acc visited node
+            | node == goal = Map.insertWith (+) node acc visited
+            | otherwise =
+                let upd_visit = Map.insertWith (+) node acc visited
+                    upd_acc = upd_visit Map.! node
+                in foldl (go upd_acc) upd_visit $ fromMaybe [] (input Map.!? node)
+-}
+
