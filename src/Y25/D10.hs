43a37463a4455507c48ff9891d72199f8bafc00f
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2025-12-13 21:20:35 +0000

    this is something, but 2 lines are not solving

--- src/Y25/D10.hs
+++ src/Y25/D10.hs
@@ -5,13 +5,10 @@ module Y25.D10
     ) where
 
 import Data.List.Split (splitOn)
-import Data.List (sortBy, nub)
-import Data.Map (Map, insert, empty, (!?))
 
 
 type Toggle = Int
 
-type Cache_Part2 = Map [Int] Int
 type ToggleArray  = [Toggle]
 type ButtonWires  = [Toggle]
 type JoltageCount = [Int]
@@ -45,25 +42,21 @@ updateToggles :: ButtonWires -> ToggleArray -> ToggleArray
 updateToggles = zipWith (\x y -> (x+y) `mod` 2)
 
 
-updateJoltage :: ButtonWires -> JoltageCount -> JoltageCount
-updateJoltage = zipWith (+)
-
-
 wireButton :: [Int] -> ToggleArray -> ToggleArray
 wireButton idxs = zipWith (\i _ -> if i `elem` idxs then 1 else 0) [0..]
 
 
 solve1line :: (ToggleArray, [ButtonWires], JoltageCount) -> Int
 solve1line (target_state, buttons, _) =
-    go 0 worst (map (const 0) target_state) buttons
+    go 0 infeasible_solution (map (const 0) target_state) buttons
     where
-        worst = 999
+        infeasible_solution = 1000000
         go :: Int -> Int -> [Toggle] -> [ButtonWires] -> Int
         go acc best tgs bts
             | acc > best = best
             | tgs == target_state =acc
             | null bts   = best
-            | otherwise  = 
+            | otherwise  =
                 go (acc+1) best (updateToggles (head bts) tgs) (tail bts)
                 `min`
                 go acc best tgs (tail bts)
@@ -73,72 +66,37 @@ solve1 :: Problem -> Answer
 solve1 = sum . map solve1line
 
 
-solve2line :: (ToggleArray, [ButtonWires], JoltageCount) -> Int
-solve2line (_, buttons, target_joltage) =
-    let init_state = map negate target_joltage
-        in_buttons = sortBy (flip compare) buttons
-        worst_best = 3 * maximum target_joltage
-    in fst $ go (0, [0], empty :: Cache_Part2) worst_best in_buttons init_state
+allToggleSolutions :: ToggleArray -> [ButtonWires] -> [[ButtonWires]]
+allToggleSolutions target_state =
+    go [] (map (const 0) target_state)
     where
-        go :: (Int, [Int], Cache_Part2) -> Int -> [ButtonWires] -> JoltageCount -> (Int, Cache_Part2)
-        go (acc, lkp_key, cache) best bts jlt
-            | acc > best = (best, cache_bst)
-            | all (==0) jlt = (acc, cache_acc)
-            | null bts = (best, cache_bst)
-            | deadend1 btn_head jlt = (best, cache_bst)
-            | deadend2 jlt = (best, cache_bst)
+        go :: [ButtonWires] -> ToggleArray -> [ButtonWires] -> [[ButtonWires]]
+        go acc tgs bts
+            | tgs == target_state = [acc]
+            | null bts = []
             | otherwise =
-                let upd_joltage = updateJoltage btn_head jlt
-                    lkp_key_next = 0:lkp_key
-                    (opt_next, upd_cache_next) = 
-                        case cache !? lkp_key_next of
-                            Just res -> (res, upd_cache_1)
-                            Nothing  -> go (acc+0, lkp_key_next, cache) best btn_tail jlt
-                    lkp_key_more = (head lkp_key + 1) : tail lkp_key
-                    (opt_more, upd_cache_more) = 
-                        case upd_cache_next !? lkp_key_more of
-                            Just res -> (res, cache)
-                            Nothing  -> go (acc+1, lkp_key_more, cache) best bts upd_joltage
-                    upd_cache = insert lkp_key opt_next upd_cache_next -- $ . insert lkp_key_next opt2 . insert lkp_key1 opt1 $ upd_cache_2
-                in (best `min` opt_next `min` opt_more, upd_cache)
-            where
-                btn_head = head bts
-                btn_tail = tail bts
-                cache_bst = insert lkp_key best cache
-                cache_acc = insert lkp_key acc cache
-        deadend1 :: ButtonWires -> JoltageCount -> Bool
-        deadend1 bws jlt =
-            let zeros = length $ takeWhile (== 0) bws
-            in any (<0) $ take zeros jlt
-        deadend2 :: JoltageCount -> Bool
-        deadend2 = any (> 0)
+                let opt1 = go (acc ++ [head bts]) (updateToggles (head bts) tgs) (tail bts)
+                    opt2 = go acc tgs (tail bts)
+                in opt1 ++ opt2
 
 
-
-presolve2line :: (ToggleArray, [ButtonWires], JoltageCount) -> Int
-presolve2line (_, buttons, target_joltage) =
-    {-
-    error $ 
-        "left side solution: " ++ show lt_sol ++ 
-        ", org target = " ++ show target_joltage ++
-        ", reduced target = " ++ show lt_joltage ++
-        ", reduced button wires = " ++ show lt_buttons
-    -- | lt_sol /= rt_sol = error $ "can't find solution for target joltage " ++ show target_joltage
-    -- | otherwise = lt_sol
-    -}
-    lt_sol
-    where 
-        maxlen = 7
-        lt_buttons = nub . filter (any (>0)) . map (take maxlen ) $ buttons 
-        lt_joltage = take maxlen target_joltage
-        lt_sol = solve2line ([], lt_buttons, lt_joltage)
-        {-
-        len = length target_joltage
-        rt_droplen = max 0 $ len - maxlen
-        rt_buttons = nub . filter (any (>0)) . map (drop rt_droplen) $ buttons 
-        rt_joltage = drop rt_droplen target_joltage
-        rt_sol = solve2line ([], rt_buttons, rt_joltage)
-        -}
+solve2line :: (ToggleArray, [ButtonWires], JoltageCount) -> Int
+solve2line (_dummy_toggles, buttons, target_joltage)
+    | all (== 0) target_joltage = 0
+    | any (< 0) target_joltage = infeasible_solution
+    | null all_solutions = infeasible_solution
+    | otherwise =
+        minimum $
+        zipWith (\solution hlf_joltage ->
+            length solution +
+            2 * solve2line (_dummy_toggles, buttons, hlf_joltage)
+        ) all_solutions hlf_joltages
+    where
+        infeasible_solution = 1000000
+        odd_toggles   = map (`mod` 2) target_joltage
+        all_solutions = allToggleSolutions odd_toggles buttons
+        rem_joltages  = map (foldl (zipWith (-)) target_joltage) all_solutions
+        hlf_joltages  = map (map (`div` 2)) rem_joltages
 
 
 solve2 :: Problem -> Answer
